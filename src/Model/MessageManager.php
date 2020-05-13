3691a3c92e41c1c08e419c91fb904491dfb14fa7
wilder4wcs <jimmy.boucaud@gmail.com>
2020-05-13 13:50:18 +0200

    Redo chat funtions_2

--- src/Model/MessageManager.php
+++ src/Model/MessageManager.php
@@ -19,8 +19,8 @@ class MessageManager extends AbstractManager
     public function insertMessage(array $message)
     {
         // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . "(conversation_id, pseudo, content, `current_date`) 
-        VALUES (1, :pseudo, :content, :current_date)");
+        $statement = $this->pdo->prepare(sprintf("INSERT INTO %s(conversation_id, pseudo, content, `current_date`) 
+        VALUES (1, :pseudo, :content, :current_date)", self::TABLE));
         $statement->bindValue('pseudo', $message['pseudo'], \PDO::PARAM_STR);
         $statement->bindValue('content', $message['content'], \PDO::PARAM_STR);
         $statement->bindValue('current_date', $message['current_date'], \PDO::PARAM_STR);
