825145262e0571b01e6dd288a66dedb78b57daed
Jules DUMAS <jules_ox@hotmail.fr>
2020-05-13 11:34:52 +0200

    modification CSS and profiles friends

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -89,21 +89,31 @@ class ProfileController extends AbstractController
         return $this->twig->render('Profile/showFriend.html.twig', [ 'result'=> $result, 'session' => $_SESSION ]);
     }
 
-    public function allUser(){
+    public function allUser()
+    {
         $profileManager = new ProfileManager();
         $users = $profileManager->selectAllFriends();
         return $this->twig->render('Profile/allUser.html.twig', ['users' => $users, 'session' => $_SESSION]);
     }
 
-    public function profileUser($id){
+    public function profileUser($id)
+    {
         $profileManager = new ProfileManager();
         $user = $profileManager->selectUserProfile($id);
         return $this->twig->render('Profile/profileUser.html.twig', ['user' => $user, 'session' => $_SESSION]);
     }
 
-    public function addFriend($id){
+    public function profileFriend($id)
+    {
+        $profileManager = new ProfileManager();
+        $friends = $profileManager->selectUserProfile($id);
+        return $this->twig->render('Profile/profileFriend.html.twig', ['friends' => $friends, 'session' => $_SESSION]);
+    }
+
+    public function addFriend($id)
+    {
         $friendManager = new FriendManager();
         $addFriend = $friendManager->addFriend($id);
-        header( 'Location: /profile/showFriend/'.$_SESSION['id']);
+        header('Location: /profile/showFriend/'.$_SESSION['id']);
     }
 }
